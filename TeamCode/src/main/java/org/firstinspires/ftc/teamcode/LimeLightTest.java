2896341ec5315212165b8dd2ddfe3c0f601d8c0b
Atharva <128868216+atharvafiveeight@users.noreply.github.com>
2025-09-13 21:10:46 -0700

    Create LimeLightTest.java
    Sahas... I made basic color tracking code for limelight... you will need to set up piplines to use (pipeline 0 )

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimeLightTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+@Autonomous
+public class LimeLightTest extends OpMode {
+private Limelight3A limelight3A;
+    @Override
+    public void init() {
+        limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight3A.pipelineSwitch(-0);
+    }
+    @Override
+    public void start() {
+        Limelight3A.start();
+    }
+    @Override
+    public void loop() {
+        LLResult llResult = limelight3A.getLatestResult();
+        if (llResult != null && llResult.isvalid()) {
+            telemetry.addData("Target X offset", llResult.getTx());
+            telemetry.addData("Target Y offset", llResult.getTy());
+            telemetry.addData("Target Area offset", llResult.getTa());
+        }
+    }
+}
